3024b5d03c1d85f3b012c66db50bcdf1dbd98e73
Gokul <gokulks.stratagile@gmail.com>
2024-04-09 17:58:06 +0530

    jsok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-// 
-document.querySelector(".slp-input__num").addEventListener('keyup', function () {
+
+document.querySelector(".slp-input__num").addEventListener("keyup", function () {
     document.querySelector(".reg-int-form__hide").style.display = "block";
 });
 
 //menu
-
 const arrOws = document.querySelectorAll(".mob-accordion");
 const List = document.querySelectorAll(".slp-menu-list");
 
