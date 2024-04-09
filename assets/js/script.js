877bf488567d5649ebd15eea98478d4279124ef8
Gokul <gokulks.stratagile@gmail.com>
2024-04-09 17:51:48 +0530

    feedback

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,17 @@
 document.querySelector(".slp-input__num").addEventListener('keyup', function () {
     document.querySelector(".reg-int-form__hide").style.display = "block";
 });
+
+//menu
+
+const arrOws = document.querySelectorAll(".mob-accordion");
+const List = document.querySelectorAll(".slp-menu-list");
+
+arrOws.forEach((item, index) => {
+
+    item.addEventListener("click", () => {
+        List[index].classList.toggle("list-show");
+    });
+    
+});
+
